da24fe961583e8a3255ba55ed43f51cd2001177b
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-02-27 13:34:46 -0700

    comit with broken pages from trying to switch to angular

--- js/houses/main-landing.component.js
+++ js/houses/main-landing.component.js
@@ -3,19 +3,16 @@
 
   angular.module('app')
     .component('mainLanding', {
-      controller: function($state, houseService) {
-        const vm = this
-
-        // vm.$onInit = function() {
-        //   vm.houses = houseService.houses
-        // }
-        //
-        // vm.addHouse = function() {
-        //   houseService.addHouse(vm.house)
-        //   $state.go('show-house', { id: vm.house.id })
-        // }
-      },
-      templateUrl: "templates/main-landing.html"
+      templateUrl: "templates/main-landing.html",
+      controller: mainLandingCtrl
     })
+
+
+  mainLandingCtrl.$inject = ['$state', 'houseService'];
+
+  function mainLandingCtrl($state, houseService) {
+    const vm = this;
+  }
+
 
 }());
